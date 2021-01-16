a68fc0ef5d5d94c5c11c344c553b7fc39d6722d7
Vyom <vyom.reloaded@gmail.com>
2021-01-16 10:24:18 -0500

    initialize the project

---
+++ src/main/java/com/gmdb/GmdbApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmdb/GmdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
